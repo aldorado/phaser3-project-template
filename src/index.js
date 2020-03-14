8ff2b900a617a652e4ccf34b49a21477541dfb4e
aldorado <anuar.suarez@hotmail.com>
2020-03-14 23:36:09 +0100

    es6 style

--- src/index.js
+++ src/index.js
@@ -1,32 +1,39 @@
 import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
 
+const WIDTH = 800;
+const HEIGHT = 600;
+
+class ExampleScene extends Phaser.Scene {
+
+  constructor() {
+    super({ key: 'ExampleScene' });
+  }
+
+  preload() {
+    this.load.image("logo", logoImg);
+  }
+  
+  create() {
+    const logo = this.add.image(400, 150, "logo");
+    
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1
+    });
+  }
+}
+
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+  width: WIDTH,
+  height: HEIGHT,
+  scene: [ExampleScene]
 };
 
 const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("logo", logoImg);
-}
-
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
